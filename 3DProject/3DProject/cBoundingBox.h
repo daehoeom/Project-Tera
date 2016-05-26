a55106975668cee929fedd71fecd239bca386a86
umdeaho <planus1992@naver.com>
2016-05-26 20:38:45 +0900

    add Bounding

--- 3DProject/3DProject/cBoundingBox.h
+++ 3DProject/3DProject/cBoundingBox.h
@@ -1,8 +1,25 @@
 #pragma once
-class cBoundingBox
+
+#include "GameObject.h"
+
+class cBoundingBox 
+	:public GameObject
 {
+	LPD3DXMESH			m_pBox;
+	D3DXMATRIXA16		m_matWorld;
+	D3DMATERIAL9		stMtl;
+	bool				m_bWireDraw;
+	D3DXVECTOR3			m_vMin;
+	D3DXVECTOR3			m_vMax;
 public:
 	cBoundingBox();
 	~cBoundingBox();
+
+	void Setup(D3DXVECTOR3* vMin, D3DXVECTOR3* vMax);
+	void Update(D3DXMATRIXA16* matWorld);
+	void Render();
+
+	D3DXVECTOR3 GetMinimum() { return m_vMin; }
+	D3DXVECTOR3 GetMaximum(){ return m_vMax; }
 };
 
